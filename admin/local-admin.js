95d541a558fab672b519174713876a1776d62f89
ngfreyman-jpg <ngfreyman@gmail.com>
2025-10-24 14:06:53 +0500

    Update local-admin.js

--- admin/local-admin.js
+++ admin/local-admin.js
@@ -7,7 +7,6 @@
 
 const CONFIG = {
   username: 'forfriends',
-  // NOTE: позже лучше поменять на SHA256("forfriends:<пароль>:ForFriends#Pepper-2025")
   passHash: 'a841ff9a9a6d1ccc1549f1acded578a2b37cf31813cd0a594ca1f1833b09d09d',
   pepper:   'ForFriends#Pepper-2025',
   tokenKey: 'ff_admin_token',
